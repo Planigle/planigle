186f2fff8823ed4b1bbb8287ed70b7f615704d3b
wbodwell <wbodwell@33a4cee0-3249-0410-921a-d980d90527ff>
2008-03-25 03:02:42 +0000

    Fixed incompatibility with later version of MySQL. Changed mail config to eliminate personal settings.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,12 +1,12 @@
 # Email settings
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {  
-  :address => "smtp-server.austin.rr.com",
+  :address => "<Replace with SMTP Server hostname>",
   :port => 25,
-  :domain => "austin.rr.com",
+  :domain => "<Replace with SMTP Server domain",
   }
 
 module MailerProperties
-  AdminEmail = 'wbodwell@austin.rr.com'    # The email address to send from
-  Site = 'www.planigle.com:3001'           # The host[:port] of the server (for URLs)
+  AdminEmail = '<Replace with Admin Email address>'    # The email address to send from
+  Site = '<Replace with web site host[:port]>'         # The host[:port] of the server (for URLs)
 end
