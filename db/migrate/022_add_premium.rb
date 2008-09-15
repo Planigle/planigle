211dd8fa1cd36d60b2937b3c8e7cb0b55dc4ccfa
wbodwell <wbodwell@33a4cee0-3249-0410-921a-d980d90527ff>
2008-09-15 15:42:47 +0000

    -Fixed issue where you couldn't change users to not be associated with a project. -Changed so that users are not premium by default.

--- db/migrate/022_add_premium.rb
+++ db/migrate/022_add_premium.rb
@@ -2,7 +2,7 @@ class AddPremium < ActiveRecord::Migration
   def self.up
     add_column :projects, :premium_expiry, :datetime
     add_column :projects, :premium_limit, :integer
-    Project.update_all :premium_expiry => Time.now + 30*24*60*60, :premium_limit => 1000
+    Project.update_all :premium_expiry => Time.now, :premium_limit => 1000
   end
 
   def self.down
