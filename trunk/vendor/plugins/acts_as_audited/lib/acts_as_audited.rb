94aaa0237d12a0547740a93ef0e7a3e832534b6b
wbodwell <wbodwell@33a4cee0-3249-0410-921a-d980d90527ff>
2009-10-08 22:48:03 +0000

    -Updated installation instructions. -Fixed upgrade to work with clean db and current code base.

--- trunk/vendor/plugins/acts_as_audited/lib/acts_as_audited.rb
+++ trunk/vendor/plugins/acts_as_audited/lib/acts_as_audited.rb
@@ -178,19 +178,24 @@ module CollectiveIdea #:nodoc:
       
         def write_audit(action = :update, user = nil)
 #          self.audits.create :changes => changed_attributes, :action => action.to_s, :user => user # WRB - Changed to look to thread for user
-          if self.class == StoryValue
-            if (story_attribute.value_type == StoryAttribute::List || story_attribute.value_type == StoryAttribute::ReleaseList) && changed_attributes['value']
-              old_value = StoryAttributeValue.find(:first, :conditions => {:id => changed_attributes['value'][0]})
-              new_value = StoryAttributeValue.find(:first, :conditions => {:id => changed_attributes['value'][1]})
-              value = [old_value ? old_value.value : '', new_value ? new_value.value : '']
+          begin
+            if self.class == StoryValue
+              if (story_attribute.value_type == StoryAttribute::List || story_attribute.value_type == StoryAttribute::ReleaseList) && changed_attributes['value']
+                old_value = StoryAttributeValue.find(:first, :conditions => {:id => changed_attributes['value'][0]})
+                new_value = StoryAttributeValue.find(:first, :conditions => {:id => changed_attributes['value'][1]})
+                value = [old_value ? old_value.value : '', new_value ? new_value.value : '']
+              else
+                value = changed_attributes['value']
+              end
+              if story
+                story.audits.create :auditable_name => story.name, :changes => {story_attribute.name => value}, :action => 'update', :project_id => Thread.current[:user] ? Thread.current[:user].project_id : nil, :user => Thread.current[:user], :username => Thread.current[:user] ? Thread.current[:user].name : nil 
+              end
             else
-              value = changed_attributes['value']
+              self.audits.create :auditable_name => name, :changes => changed_attributes, :action => action.to_s, :project_id => Thread.current[:user] ? Thread.current[:user].project_id : nil, :user => Thread.current[:user], :username => Thread.current[:user] ? Thread.current[:user].name : nil 
             end
-            if story
-              story.audits.create :auditable_name => story.name, :changes => {story_attribute.name => value}, :action => 'update', :project_id => Thread.current[:user] ? Thread.current[:user].project_id : nil, :user => Thread.current[:user], :username => Thread.current[:user] ? Thread.current[:user].name : nil 
-            end
-          else
-            self.audits.create :auditable_name => name, :changes => changed_attributes, :action => action.to_s, :project_id => Thread.current[:user] ? Thread.current[:user].project_id : nil, :user => Thread.current[:user], :username => Thread.current[:user] ? Thread.current[:user].name : nil 
+          rescue Exception => e
+            logger.error(e)
+            logger.error(e.backtrace.join("\n"))
           end
         end
 
