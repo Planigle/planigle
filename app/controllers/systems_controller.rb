fa43a8b0502f58ee448dc6a163bcd3aec7cf2e95
wbodwell <wbodwell@33a4cee0-3249-0410-921a-d980d90527ff>
2010-10-18 02:28:40 +0000

    -Optimize report data fetching so that we only get the recent iterations first.  Then we do the earlier iterations. -Clean up job to update priorities.

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -17,6 +17,15 @@ class SystemsController < ResourceController
     end
   end
 
+  # Return the system data.
+  # GET /report_remaining
+  def report_remaining
+    respond_to do |format|
+      format.xml { render :xml => data_remaining, :status => :success }
+      format.amf { render :amf => data_remaining }
+    end
+  end
+
 protected
 
   # Return all records.
@@ -65,15 +74,36 @@ protected
     end
   end
   
-  # Answer the reporting data
+  # Answer the reporting data for the last 4 iterations and last release.
   def data
     report_data = {}
-    report_data['iteration_totals'] = IterationTotal.find(:all, :conditions => ['iterations.project_id = ?', project_id], :joins => :iteration)
-    report_data['release_totals'] = ReleaseTotal.find(:all, :conditions => ['releases.project_id = ?', project_id], :joins => :release)
-    report_data['iteration_breakdowns'] = Iteration.find(:all, :include => [{:stories => :story_values},:project], :conditions => {:project_id => project_id}).inject([]) {|collect, iteration| collect.concat(CategoryTotal.summarize_for(iteration))}
-    report_data['release_breakdowns'] = Release.find(:all, :conditions => {:project_id => project_id}).inject([]) {|collect, release| collect.concat(CategoryTotal.summarize_for(release))}
-    report_data['iteration_story_totals'] = IterationStoryTotal.find(:all, :conditions => ['iterations.project_id = ?', project_id], :joins => :iteration)
+    last4Iterations = Iteration.find(:all, :limit => 4, :order => 'start desc', :include => [{:stories => :story_values},:project], :conditions => ['iterations.project_id = ? and start < now()', project_id])
+    last4IterationIds = last4Iterations.collect{|iteration| iteration.id}
+    lastRelease = Release.find(:all, :limit => 1, :order => 'start desc', :conditions => ['releases.project_id = ? and start < now()', project_id])
+    lastReleaseIds = lastRelease.collect{|release| release.id}
+    report_data['iteration_totals'] = IterationTotal.find(:all, :conditions => ['iterations.id in (?)', last4IterationIds], :joins => :iteration)
+    report_data['iteration_story_totals'] = IterationStoryTotal.find(:all, :conditions => ['iterations.id in (?)', last4IterationIds], :joins => :iteration)
+    report_data['release_totals'] = ReleaseTotal.find(:all, :conditions => ['releases.id in (?)', lastReleaseIds], :joins => :release)
+    report_data['iteration_breakdowns'] = last4Iterations.inject([]) {|collect, iteration| collect.concat(CategoryTotal.summarize_for(iteration))}
+    report_data['release_breakdowns'] = lastRelease.inject([]) {|collect, release| collect.concat(CategoryTotal.summarize_for(release))}
     report_data['iteration_velocities'] = IterationVelocity.find(:all, :conditions => ['iterations.project_id = ?', project_id], :joins => :iteration)
+    report_data
+  end
+  
+  # Answer the reporting data for the remaining iterations and releases.
+  def data_remaining
+    report_data = {}
+    remainingIterations = Iteration.find(:all, :order => 'start desc', :include => [{:stories => :story_values},:project], :conditions => ['iterations.project_id = ? and start < now()', project_id])
+    remainingIterations = remainingIterations.length > 4 ? remainingIterations[4,remainingIterations.length] : []
+    remainingIterationIds = remainingIterations.collect{|iteration| iteration.id}
+    remainingReleases = Release.find(:all, :order => 'start desc', :conditions => ['releases.project_id = ? and start < now()', project_id])
+    remainingReleases = remainingReleases.length > 1 ? remainingReleases[1,remainingReleases.length] : []
+    remainingReleaseIds = remainingReleases.collect{|iteration| iteration.id}
+    report_data['iteration_totals'] = IterationTotal.find(:all, :conditions => ['iterations.id in (?)', remainingIterationIds], :joins => :iteration)
+    report_data['iteration_story_totals'] = IterationStoryTotal.find(:all, :conditions => ['iterations.id in (?)', remainingIterationIds], :joins => :iteration)
+    report_data['release_totals'] = ReleaseTotal.find(:all, :conditions => ['releases.id in (?)', remainingReleaseIds], :joins => :release)
+    report_data['iteration_breakdowns'] = remainingIterations.inject([]) {|collect, iteration| collect.concat(CategoryTotal.summarize_for(iteration))}
+    report_data['release_breakdowns'] = remainingReleases.inject([]) {|collect, release| collect.concat(CategoryTotal.summarize_for(release))}
     report_data
   end
 end
